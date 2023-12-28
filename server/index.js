d50bd0342e5fc0e9e7f8050763065fa60eef5be2
HIMANSHU RAI <himanshuuu.2001@gmail.com>
2023-12-28 11:00:20 +0530

    backend deploy

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
 import { Server } from "socket.io";
+import  express  from "express";
+import cors from "cors";
+import { createServer } from "http";
 import Connection from "./database/db.js";
 import { getDocument,updateDocument } from "./controller/document-controller.js";
 
 const PORT = process.env.PORT||9000;
 
 Connection();
-const io = new Server(PORT,{
-    cors:{
-        origin:'http://localhost:3000',
-        methods:['GET','POST']
-    }
-});
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+httpServer.listen(PORT,()=>{
+    console.log("SERVER RUNNING");
+})
+// const io = new Server(PORT,{
+//     cors:{
+//         origin:'http://localhost:3000',
+//         methods:['GET','POST']
+//     }
+// });
+const io = new Server(httpServer);
 
 io.on('connection',socket=>{
+    console.log("Connected");
     socket.on('get-document',async documentId=>{
         const document = await getDocument(documentId);
         socket.join(documentId);
